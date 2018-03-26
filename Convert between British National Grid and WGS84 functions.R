b21183f994560cb9deb7ccd3e235e92e67336df9
Michael Sinclair <ihaveknowledge@yahoo.co.uk>
2018-03-26 12:32:42 +0100

    Convert between BNG and WGS84

---
+++ Convert between British National Grid and WGS84 functions.R
@@ -0,0 +1,24 @@
+#-------British National Grid to WGS84-----------------
+bng_to_wgs84 <- function(datain){
+  library(rgdal) 
+  bng = datain 
+  coordinates(bng) = ~ e + n 
+  bng@proj4string = CRS("+init=epsg:27700") 
+  latlon = spTransform(bng, CRS("+proj=longlat +datum=WGS84")) 
+  out = cbind(datain, coordinates(latlon)) 
+  colnames(out) = c(names(datain),'lon','lat') 
+  return(out)
+}
+
+#-------WGS84 to British National Grid------------------
+
+wgs_84_to_bng <- function(datain){
+  library(rgdal) 
+  bng = datain 
+  coordinates(bng) = ~ lon + lat 
+  bng@proj4string = CRS("+proj=longlat +datum=WGS84") 
+  latlon = spTransform(bng, CRS("+init=epsg:27700")) 
+  out = cbind(datain, coordinates(latlon)) 
+  colnames(out) = c(names(datain),'e','n') 
+  return(out)
+}
